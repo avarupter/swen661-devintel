707ec973e16b77a7def61c07a085bb22dc87befd
avarupter <avarupter@gmail.com>
2026-08-16 15:24:23 +0900

    Add Hello World apps (node_modules excluded)

---
+++ hello-react-web/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontSize: '32px'
+    }}>
+      Hello, World!
+    </div>
+  );
+}
+
+export default App;
